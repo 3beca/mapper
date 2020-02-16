96bd784591d4309de1fff070b10af1c115604585
Juanjo <juanjo@juanjofp.com>
2020-02-16 20:01:25 +0100

    skip test without nock

--- __tests__/route-mappers.e2e.js
+++ __tests__/route-mappers.e2e.js
@@ -167,7 +167,7 @@ describe(
                     payload: {name: 'influxDB-write', template: 'cpu_load_short,host=server01,region=us-west value={{body.value}} {{body.timestamp}}'}
                 });
                 const mapping = JSON.parse(responseMapping.payload);
-                // curl -i -XPOST 'http://localhost:8086/write?db=tribeca' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
+                // Create target
                 const responseTarget = await server.inject({
                     method: 'POST',
                     headers: {'content-type': 'application/json'},
@@ -209,5 +209,5 @@ describe(
     }
 );
 
-
+// curl -i -XPOST 'http://localhost:8086/write?db=tribeca' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
 // curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=tribeca" --data-urlencode "q=SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west'"
